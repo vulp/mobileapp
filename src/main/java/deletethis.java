d860a505be58db42c7fceb8f77c830a00a00152d
vulp <tuomo.pohjola@gmail.com>
2012-08-11 15:28:34 +0300

    first class adding test

---
+++ src/main/java/deletethis.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: vulp
+ * Date: 8/11/12
+ * Time: 3:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class deletethis {
+}
